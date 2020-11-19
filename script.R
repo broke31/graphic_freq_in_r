d34f62461837a07066720c05dfc3dc7f4a18610b
broke31 <giammagiorda@gmail.com>
2020-11-19 13:12:56 +0100

    first commit

---
+++ script.R
@@ -0,0 +1,36 @@
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+read_file_cvs <- function(NameFile,is_read_col_name){
+  frequencies <- read_csv(NameFile, col_names = is_read_col_name)
+  df <-data.frame(frequencies)
+  return (df)
+}
+
+get_freq_from_df<- function(df){
+  df_with_freq <- as.data.frame(table(unlist(df)))
+  return (df_with_freq)
+}
+
+plot_df <- function(df_with_freq, x_lab,y_lab){
+  ggplot(df_with_freq,aes(x= reorder(Var1,-Freq),Freq)) +
+    geom_bar(stat ="identity") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+    geom_text(aes(label = signif(Freq, digits = 3)), nudge_y = 1) + 
+    labs(title="",
+         x=x_lab, y= y_lab)
+}
+
+
+main <- function(){
+  NameFile <- "Desktop/graphicR/Keyword.csv"
+  x_lab = "Keywords"
+  y_lab = "Frequencies"
+  is_read_col_name = F
+  
+  df <- read_file_cvs(NameFile = NameFile, is_read_col_name = is_read_col_name)
+  df_with_freq <- get_freq_from_df(df)
+  plot_df(df_with_freq,x_lab,y_lab)
+  
+}
