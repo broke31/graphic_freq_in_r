fa0b25b436943ff3a8fb53381909524d22dba823
broke31 <giammagiorda@gmail.com>
2020-11-20 11:44:45 +0100

    add folder figures and Keywords

--- script.R
+++ script.R
@@ -14,12 +14,12 @@ get_freq_from_df<- function(df){
 }
 
 plot_df <- function(df_with_freq, x_lab,y_lab){
-  ggplot(df_with_freq,aes(x= reorder(Var1,-Freq),Freq)) +
-    geom_bar(stat ="identity") +
+  ggplot(df_with_freq,mapping = aes(x= reorder(Var1,Freq), Freq)) +
+    geom_bar(stat ="identity") + coord_flip()+
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-    geom_text(aes(label = signif(Freq, digits = 3)), nudge_y = 2) + 
+    geom_text(aes(label = signif(Freq, digits = 3)), nudge_y = 1) + 
     labs(title="",
-         x=x_lab, y= y_lab)
+         x=x_lab, y= y_lab) 
 }
 
 
@@ -30,8 +30,10 @@ main <- function(){
   is_read_col_name = F
   
   df <- read_file_cvs(NameFile = NameFile, is_read_col_name = is_read_col_name)
+  print(length(df))
   df_with_freq <- get_freq_from_df(df)
-  plot_df(df_with_freq,x_lab,y_lab)
   print(length(df_with_freq$Var1))
+  plot_df(df_with_freq,x_lab,y_lab)
+  
 
 }
